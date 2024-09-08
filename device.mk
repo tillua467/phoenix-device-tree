afe36c404382b218bb8d9940cfdfb2b695ad5826
Gagan Malvi <malvigagan@gmail.com>
2024-09-08 10:52:48 +0530

    phoenix: Compile liblzma for goodix
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    libkeymaster_messages.vendor:64
+    libkeymaster_messages.vendor:64 \
+    liblzma.vendor:64
 
 # Input
 PRODUCT_COPY_FILES += \
